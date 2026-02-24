a99184385d11ee956faa2629bf1591875d00c723
Philip Andrew Wee De Wang <philip.andrewweedewang@gmail.com>
2026-02-24 09:24:49 +0800

    remove sentry from postgres meta

--- src/server/sentry.ts
+++ src/server/sentry.ts
@@ -1,5 +1,4 @@
 import * as Sentry from '@sentry/node'
-import { nodeProfilingIntegration } from '@sentry/profiling-node'
 
 const sentryEnvironment = process.env.ENVIRONMENT ?? 'local'
 const dsn = process.env.SENTRY_DSN ?? ''
@@ -7,14 +6,10 @@ const dsn = process.env.SENTRY_DSN ?? ''
 const captureOptions: Sentry.NodeOptions =
   sentryEnvironment === 'prod'
     ? {
-        // Tracing
-        tracesSampleRate: 0.00001, // trace 1/10k events
-        // Set sampling rate for profiling - this is evaluated only once per SDK.init call
-        profilesSampleRate: 0.00001, // profile 1/10k events
+        tracesSampleRate: 0.00001,
       }
     : {
-        tracesSampleRate: 0.01, // trace 1% of the events
-        profilesSampleRate: 0.01,
+        tracesSampleRate: 0.01,
       }
 
 const sensitiveKeys = ['pg', 'x-connection-encrypted']
@@ -33,7 +28,7 @@ export default Sentry.init({
   enabled: Boolean(dsn),
   dsn: dsn,
   environment: sentryEnvironment,
-  integrations: [nodeProfilingIntegration()],
+  integrations: [],
   beforeSendTransaction(transaction) {
     if (transaction.contexts?.trace?.data) {
       redactSensitiveData(transaction.contexts.trace.data)
